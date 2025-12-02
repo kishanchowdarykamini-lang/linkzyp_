282ef9aad7bbaa896f7e7133647b312b0f05c699
kishanchowdarykamini-lang <kishanchowdarykamini@gmail.com>
2025-12-02 18:12:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'https://musical-paletas-a0f22a.netlify.app',
+    credentials: true
+}));
+// Basic test route
+const authRoutes = require("./routes/authRoutes");
+app.use("/auth", authRoutes);
+
+const linkRoutes = require("./routes/linkRoutes");
+app.use("/links", linkRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Linkzyp Backend is Running");
+});
+
+// Connect to MongoDB (will add URL later)
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log("DB Error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
